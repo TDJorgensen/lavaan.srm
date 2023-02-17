1a5f2c920153ff7a44ccf22d8125ce56d319e7aa
TDJorgensen <TJorgensen314@gmail.com>
2023-02-17 13:10:05 +0100

    remove lavaan.srm alias from package page

--- R/lavaan.srm-package.R
+++ R/lavaan.srm-package.R
@@ -1,16 +1,15 @@
-#' The 'lavaan.srm' package.
-#'
-#' @description A DESCRIPTION OF THE PACKAGE
-#'
-#' @docType package
-#' @name lavaan.srm-package
-#' @aliases lavaan.srm
-#' @useDynLib lavaan.srm, .registration = TRUE
-#' @import methods
-#' @import Rcpp
-#' @importFrom rstan sampling
-#'
-#' @references
-#' Stan Development Team (2022). RStan: the R interface to Stan. R package version 2.21.7. https://mc-stan.org
-#'
+##' The 'lavaan.srm' package.
+##'
+##' @description A DESCRIPTION OF THE PACKAGE
+##'
+##' @docType package
+##' @name lavaan.srm-package
+##' @useDynLib lavaan.srm, .registration = TRUE
+##' @import methods
+##' @import Rcpp
+##' @importFrom rstan sampling
+##'
+##' @references
+##' Stan Development Team (2022). RStan: the R interface to Stan. R package version 2.21.7. https://mc-stan.org
+##'
 NULL
