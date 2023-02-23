82c7101f3777d825b67fd58805c393d1cb587c91
TDJorgensen <TJorgensen314@gmail.com>
2023-02-23 13:23:39 +0100

    acknowledge Yves and NWO contributions

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,8 @@
     packageStartupMessage("This is ", paste(pkgname, version), ", a package for fitting")
     packageStartupMessage("structural equation models to round-robin data.")
     packageStartupMessage("See the NEWS file for latest features, bug reports.")
+    packageStartupMessage(pkgname, " was first developed at the University of Amsterdam")
+    packageStartupMessage("partially funded by NWO grants 016.Veni.195.457 and 406.XS.01.078")
     packageStartupMessage("###################################################################")
 }
 
